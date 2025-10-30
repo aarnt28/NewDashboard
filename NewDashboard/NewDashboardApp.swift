3c2884e0c325b3f20b417e72bd6751578012129e
Aaron Turner <aaron@vipclouds.com>
2025-10-30 16:45:00 -0500

    Initial Commit

---
+++ NewDashboard/NewDashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewDashboardApp.swift
+//  NewDashboard
+//
+//  Created by Aaron Turner on 10/30/25.
+//
+
+import SwiftUI
+
+@main
+struct NewDashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
